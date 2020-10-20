a2526cfb262dfc2da330f46af0bfe740aa4ff11f
Paulo Gabriel <pgj1085@gmail.com>
2020-10-20 14:27:59 -0300

    Initial Commit

---
+++ src/test/java/com/pgbezerra/bezerras/BezerrasRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgbezerra.bezerras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezerrasRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
