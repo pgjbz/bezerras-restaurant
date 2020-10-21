7f8503c921fbbcdfa4205abc591d0fa487eed1ee
Paulo Gabriel <pgj1085@gmail.com>
2020-10-20 22:02:57 -0300

    Product repository and her tests

---
+++ src/main/java/com/pgbezerra/bezerras/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.pgbezerra.bezerras.repository;
+
+import com.pgbezerra.bezerras.entities.model.Product;
+
+public interface ProductRepository extends Repository<Product, Integer> {
+
+}
---
+++ src/test/java/com/pgbezerra/bezerras/repository/ProductRepositoryTest.java
@@ -0,0 +1,190 @@
+package com.pgbezerra.bezerras.repository;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.pgbezerra.bezerras.entities.model.Category;
+import com.pgbezerra.bezerras.entities.model.Product;
+import com.pgbezerra.bezerras.repository.exception.DatabaseException;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = Replace.ANY)
+public class ProductRepositoryTest {
+
+	private static final List<Category> categories = new ArrayList<>();
+	private static final List<Product> products = new ArrayList<>();
+	
+	@Autowired
+	private ProductRepository productRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	
+	{
+		Category c1 = new Category(null, "Food");
+		Category c2 = new Category(null, "Drink");
+		Product p1 = new Product(null, "Feijoada", BigDecimal.valueOf(25.0), c1);
+		Product p2 = new Product(null, "Beer", BigDecimal.valueOf(25.0), c2);
+		categories.addAll(Arrays.asList(c1, c2));
+		products.addAll(Arrays.asList(p1, p2));
+	}
+	
+	@Test(expected = DatabaseException.class)
+	public void insertProductWithouCategoryExpectedError() {
+		Product product = new Product(null, "Feijoada", BigDecimal.valueOf(25.0), null);
+		productRepository.insert(product);
+	}
+	
+	@Test(expected = DatabaseException.class)
+	public void inserProductWithoutCategoryIdExpectedError() {
+		Category category = new Category(null, "Food");
+		Product product = new Product(null, "Feijoada", BigDecimal.valueOf(25.0), category);
+		productRepository.insert(product);
+	}
+	
+	@Test(expected = DatabaseException.class)
+	public void inserProductWithInexistentCategoryExpetecSuccess() {
+		Category category = new Category(1, "Food");
+		Product product = new Product(null, "Feijoada", BigDecimal.valueOf(25.0), category);
+		productRepository.insert(product);
+	}
+	
+	@Test
+	public void insertProductExpectedSuccess() {
+		Category category = new Category(null, "Food");
+		categoryRepository.insert(category);
+		Product product = new Product(null, "Feijoada", BigDecimal.valueOf(25.0), category);
+		productRepository.insert(product);
+	}
+	
+	@Test(expected = DatabaseException.class)
+	public void updateProductWithouNameExpectedError() {
+		Category category = new Category(null, "Food");
+		categoryRepository.insert(category);
+		Product product = new Product(null, "Feijoada", BigDecimal.valueOf(25.0), category);
+		productRepository.insert(product);
+		product.setName(null);
+		productRepository.update(product);
+	}
+	
+	@Test(expected = DatabaseException.class)
+	public void updateProductWithoutValueExpectedError() {
+		Category category = new Category(null, "Food");
+		categoryRepository.insert(category);
+		Product product = new Product(null, "Feijoada", BigDecimal.valueOf(25.0), category);
+		productRepository.insert(product);
+		product.setValue(null);
+		productRepository.update(product);
+	}
+	
+	@Test(expected = DatabaseException.class)
+	public void updateProductWithoutCategoryExpectedError() {
+		Category category = new Category(null, "Food");
+		categoryRepository.insert(category);
+		Product product = new Product(null, "Feijoada", BigDecimal.valueOf(25.0), category);
+		productRepository.insert(product);
+		product.setCategory(null);
+		productRepository.update(product);
+	}
+	
+	@Test(expected = DatabaseException.class)
+	public void updateProductWithoutAllValuesExpectedError() {
+		Category category = new Category(null, "Food");
+		categoryRepository.insert(category);
+		Product product = new Product(null, "Feijoada", BigDecimal.valueOf(25.0), category);
+		productRepository.insert(product);
+		product.setName(null);
+		product.setValue(null);
+		product.setCategory(null);
+		productRepository.update(product);
+	}
+	
+	@Test(expected = DatabaseException.class)
+	public void updateProductWithInexistentCategoryExpectedError() {
+		Category category = new Category(null, "Food");
+		categoryRepository.insert(category);
+		Product product = new Product(null, "Feijoada", BigDecimal.valueOf(25.0), category);
+		productRepository.insert(product);
+		category.setId(999);
+		productRepository.update(product);
+	}
+	
+	@Test
+	public void updateProductExpectedSuccess() {
+		Category category = new Category(null, "Food");
+		categoryRepository.insert(category);
+		Product product = new Product(null, "Feijoada", BigDecimal.valueOf(25.0), category);
+		productRepository.insert(product);
+		product.setName("Baiao de Dois");
+		Assert.assertTrue(productRepository.update(product));
+	}
+	
+	@Test
+	public void updateInexistentProductExpectedNoUpdates() {
+		Product product = new Product(999, "Feijoada", BigDecimal.valueOf(25.0), null);
+		product.setName("Baiao de Dois");
+		Assert.assertFalse(productRepository.update(product));
+	}
+	
+	@Test
+	public void deleteProductExpectedNoSuccess() {
+		Assert.assertFalse(productRepository.deleteById(999));
+	}
+	
+	@Test
+	public void deleteProductExpectedSuccess() {
+		Category category = new Category(null, "Food");
+		categoryRepository.insert(category);
+		Product product = new Product(null, "Feijoada", BigDecimal.valueOf(25.0), category);
+		productRepository.insert(product);
+		Assert.assertTrue(productRepository.deleteById(product.getId()));
+	}
+	
+	@Test
+	public void findAllExpectedNoReturn() {
+		List<Product> products = productRepository.findAll();
+		Assert.assertEquals(0, products.size());
+	}
+	
+	@Test
+	public void findAllExpectReturn() {
+		categoryRepository.insertAll(ProductRepositoryTest.categories);
+		productRepository.insertAll(ProductRepositoryTest.products);
+		List<Product> products = productRepository.findAll();
+		Assert.assertNotEquals(0, products.size());
+	}
+	
+	@Test
+	public void findByIdInexsistentProductExpectedError() {
+		Optional<Product> product = productRepository.findById(999);
+		Assert.assertFalse(product.isPresent());
+	}
+	
+	@Test
+	public void findByIdExpectedSuccess() {
+		Category category = new Category(null, "Food");
+		categoryRepository.insert(category);
+		Product product = new Product(null, "Feijoada", BigDecimal.valueOf(25.0), category);
+		productRepository.insert(product);
+		Optional<Product> productReturn = productRepository.findById(product.getId());
+		Assert.assertTrue(productReturn.isPresent());
+	}
+	
+
+}
