07e9fc727bf8f2e8c6dda7d4bd08e62f80f0a81d
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2020-12-12 17:00:01 -0300

    Return BADREQUEST on database error

--- src/main/java/com/pgbezerra/bezerras/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/pgbezerra/bezerras/resources/exception/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.pgbezerra.bezerras.resources.exception;
 
+import com.pgbezerra.bezerras.repository.exception.DatabaseException;
 import com.pgbezerra.bezerras.services.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,14 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> notFound(ResourceNotFoundException ex, HttpServletRequest request){
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError standardError = new StandardError(LocalDateTime.now(), status.value(), error, ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(standardError);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(ResourceNotFoundException ex, HttpServletRequest request){
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError standardError = new StandardError(LocalDateTime.now(), status.value(), error, ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(standardError);
     }
