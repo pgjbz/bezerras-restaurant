7f492ccb23cd581ae6d5f0e6b18400c69be0f7f7
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2021-01-26 21:58:01 -0300

    Login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormLoginComponent } from './modules/login/components/form-login/form-login.component';
+import { PageLoginComponent } from './modules/login/pages/page-login/page-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageLoginComponent,
     FormLoginComponent
   ],
   imports: [
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormLoginComponent } from './components/form-login/form-login.component';
 import { FormsModule } from '@angular/forms';
+import { PageLoginComponent } from './pages/page-login/page-login.component';
 
 @NgModule({
-  declarations: [FormLoginComponent],
+  declarations: [FormLoginComponent, PageLoginComponent],
   imports: [
     CommonModule,
     FormsModule
